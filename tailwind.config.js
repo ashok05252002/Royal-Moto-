80530832a7f0e9ae24c5c7cf86ef62a8684da7be
ashok05252002 <ashokkumarm25052002@gmail.com>
2024-06-19 21:15:56 +0530

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "/src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#ff77e9',     
+       
+      },
+    },
+  },
+  plugins: [],
+}
+
